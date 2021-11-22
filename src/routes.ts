cc96e9cce4c43263f5d1bd59a55a1b381969c994
Niklas Lavrell <niklas@grapic.co>
2021-11-22 12:12:33 +0100

    build: inject env variable

--- src/routes.ts
+++ src/routes.ts
@@ -6,14 +6,11 @@ export const NEW_GRAPIC_EMBED_FIGMA = `/new/figmaEmbed`;
 export const QUERY_AUTO_SIGN_IN = `authenticate`;
 export const QUERY_AUTO_SIGN_IN_ANONYMOUS = `anonymous`;
 
-export const baseUrl =
-  "https://grapic--pr31-feat-figma-plugin-pw7m8tlx.web.app";
-// export const baseUrl = "https://app.grapic.co"
-// export const baseUrl = "https://staging.app.grapic.co";
-// export const baseUrl = "http://localhost:3000";
+declare var process: { env?: "production" | "development" } | undefined;
 
-// TODO: fix this to proper URL
-// export const baseUrl =
-//   process.env.REACT_APP_RELEASE_ENV === "production"
-//     ? "https://app.grapic.co/"
-//     : window.location.origin;
+export const baseUrl =
+  process?.env === "production"
+    ? "https://app.grapic.co"
+    : // : "https://staging.app.grapic.co";
+      // : "http://localhost:3000";
+      "https://grapic--pr31-feat-figma-plugin-pw7m8tlx.web.app";
