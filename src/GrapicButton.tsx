ee5ca9f2d2a1906e18453b6a3727b006f561f045
Niklas Lavrell <niklas@grapic.co>
2021-12-08 13:26:35 +0100

    feat: added drop shadow to button

--- src/GrapicButton.tsx
+++ src/GrapicButton.tsx
@@ -16,6 +16,12 @@ const GrapicButton: AutoLayout = (props) => {
         right: figmaUtils.remToPx(1.5),
       }}
       cornerRadius={figmaUtils.remToPx(0.6)}
+      effect={{
+        blur: 10,
+        color: { r: 0, g: 0, b: 0, a: 0.07 },
+        offset: { x: 1, y: 4 },
+        type: "drop-shadow",
+      }}
       {...props}
     >
       <Text
